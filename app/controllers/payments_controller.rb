e4568b882c3066d590c5c60ae4bc9eb4906b1924
shubhambdobariya <shubhamd.essence@gmail.com>
2024-07-18 15:08:51 +0530

    pushed the stripe keys

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,33 +1,42 @@
-Class PaymentsController < ApplicationController
+class PaymentsController < ApplicationController
+
   def create
-    #create stripe customer for payment, update if already created
-    customer = Stripe::Customer.create(
-      name: current_user.firstname,
-      email: current_user.email,
-      description: "Customer id: #{current_user.id}",
-    )
+    p 44444444444444444444
+    p current_user
+    p 44444444444444444444
+      customer = Stripe::Customer.create(
+        name: current_user.firstname,
+        email: current_user.email,
+        description: "Customer id: #{current_user.id}",
+      )
   
-    session = Stripe::Checkout::Session.create( 
-      customer: customer, 
-      payment_method_types: ['card'],
-      line_items: [{
-        price: 'price_1234', #price api id usually starts with price_ApIiD
-        quantity: 1,
-      }],
-      mode: 'subscription',
-      success_url:  payments_success_url,
-      cancel_url: payments_cancel_url
-     )
-     redirect_to session.url
+      session = Stripe::Checkout::Session.create( 
+        customer: customer.id, 
+        payment_method_types: ['card'],
+        line_items: [{
+          price: 'price_1234', # price API id usually starts with price_ApIiD
+          quantity: 1,
+        }],
+        mode: 'subscription',
+        success_url: payments_success_url,
+        cancel_url: payments_cancel_url
+      )
+      redirect_to session.url
+  end
+
+  private
+
+  def set_stripe_api_key
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
   end
   
   def success
-    #handle successful payments
+    # Handle successful payments
     redirect_to root_url, notice: "Purchase Successful"
   end
   
   def cancel
-    #handle if the payment is cancelled
+    # Handle if the payment is cancelled
     redirect_to root_url, notice: "Purchase Unsuccessful"
   end
 end
